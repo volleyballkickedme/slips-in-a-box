34a7469e30f50a32d02aeffd3cd38ba562509c7a
volleyballkickedme <89293409+volleyballkickedme@users.noreply.github.com>
2024-10-01 23:56:07 +0800

    Fix bug that caused same location to be generated on the first click after clearing display

--- src/Components/Generate.jsx
+++ src/Components/Generate.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
 import { useState } from 'react'
 
-const Generate = ( { locationsArray }) => {
+const Generate = ( { locationsArray, poolSize }) => {
   const[output, setOutput] = useState('')
   const[index, setIndex] = useState(0)
-  const poolSize = locationsArray.length 
 
   //function to generate random number
   function getRandomNumber(min, max) {  
@@ -18,14 +17,19 @@ const Generate = ( { locationsArray }) => {
     setOutput(locationsArray[index].name)
   }
 
+  const handleClear = () => {
+    setOutput('')
+    setIndex(getRandomNumber(0, poolSize))
+  }
+
   return (
     <section className='flex flex-col items-center justify-center'>
         <div className='flex gap-5 bg-indigo-200 px-10 py-2 px-24 rounded-lg'>
           <div className='flex flex-col gap-3'>
             <button className='bg-blue-100 px-2 py-1 rounded-lg'
-              onClick={handleClick}>Generate</button>
+              onClick={ handleClick }>Generate</button>
             <button className='bg-blue-300 px-2 py-1 rounded-lg'
-            onClick={() => setOutput('')}>Clear</button>
+            onClick={ handleClear }>Clear</button>
           </div>
           
           <div className='flex flex-col justify-center align-center'>
